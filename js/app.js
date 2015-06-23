24bd7f6a28133643df67439b7c61211a386ec0c0
Radha <k.radha@qeemah.com>
2015-06-23 13:47:22 +0400

    initial files

---
+++ js/app.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+	$('#tab-bar').find('a').on('click', function(e){
+		console.log("some one clicked me");
+		e.preventDefault();
+		var nextPage = $(e.target.hash);
+		page(nextPage); //You need to add this for it to work
+		$("#pages").find(".current").removeClass("current");
+		nextPage.addClass("current");
+	});
+
+	function page(toPage) {
+		var toPage = $(toPage),
+		fromPage = $("#pages").find(".current");
+		if(toPage.hasClass("current") || toPage === fromPage) {
+		return;
+		}
+		toPage.addClass("current fade in").one("webkitAnimationEnd", function(){
+		fromPage.removeClass("current fade out");
+		toPage.removeClass("fade in")
+		});
+		fromPage.addClass("fade out");
+	}
+})
